efcda09ac95dc1e4075c6dad83d938d80d4cad08
maxiberosich <mberosich@gmail.com>
2023-10-24 19:33:11 -0300

    Creacion Proyecto

---
+++ src/main/java/openHouse/demo/OpenHouseApplication.java
@@ -0,0 +1,13 @@
+package openHouse.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenHouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/openHouse/demo/OpenHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package openHouse.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
