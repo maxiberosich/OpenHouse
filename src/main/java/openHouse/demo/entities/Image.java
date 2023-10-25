aa3cd6d28cdf10b085f53630b3f1b5b2a0d6f281
Fernán Pietrantuono <fernanpietrantuono@gmail.com>
2023-10-25 17:50:01 -0300

    Agrego la clase Imagen

---
+++ src/main/java/openHouse/demo/entities/Image.java
@@ -0,0 +1,25 @@
+package openHouse.demo.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+public class Image {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    private String name;
+    private byte[] content;
+
+}
