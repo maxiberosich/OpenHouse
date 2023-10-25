cdcabcb810c41d3aed83db4f8dc4ba500fd3f434
“Laura <laurazeier@gmail.com>
2023-10-25 16:56:10 -0300

    Realicé un trackeo de rama developer y agregué una entidad user

---
+++ src/main/java/openHouse/demo/entities/User.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package openHouse.demo.entities;
+
+import jakarta.persistence.Entity;
+
+/**
+ *
+ * @author laura
+ */
+@Entity
+public class User {
+    private String nombre; 
+    
+}
