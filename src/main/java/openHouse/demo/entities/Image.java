61fd555630e31e8de612a28707199cb85bbb2cf0
Fernán Pietrantuono <fernanpietrantuono@gmail.com>
2023-10-26 10:29:26 -0300

    Agrego las clases correspondientes para imagen. Faltan las otras clases para completar y agregar métodos que necesitan de las otras clases

--- src/main/java/openHouse/demo/entities/Image.java
+++ src/main/java/openHouse/demo/entities/Image.java
@@ -1,16 +1,20 @@
 package openHouse.demo.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Data
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 public class Image {
@@ -19,7 +23,10 @@ public class Image {
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
+    private String mime;
     private String name;
+    @Lob
+    @Column(columnDefinition = "LONGBLOB")
     private byte[] content;
 
 }
---
+++ src/main/java/openHouse/demo/repositories/ImageRepository.java
@@ -0,0 +1,10 @@
+package openHouse.demo.repositories;
+
+import openHouse.demo.entities.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImageRepository extends JpaRepository<Image, String> {
+
+}
