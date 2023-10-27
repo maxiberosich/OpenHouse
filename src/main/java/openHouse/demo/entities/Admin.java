0f603c4390149862d3472e53e8e08db14c1c844f
“Jhoel <pkap1995@gmail.com>
2023-10-27 00:54:30 -0300

    Entidad Property y el servicio ,faltan cosas.

--- src/main/java/openHouse/demo/entities/Admin.java
+++ src/main/java/openHouse/demo/entities/Admin.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+//data de lombok ------viene con los get and setter y to sting.Constructores tambien vacio y completo.
 @Entity
 @Data
 @NoArgsConstructor
---
+++ src/main/java/openHouse/demo/services/PropertyService.java
@@ -0,0 +1,66 @@
+package openHouse.demo.services;
+
+import openHouse.demo.entities.Property;
+import openHouse.demo.exception.MiException;
+import openHouse.demo.repositories.PropertyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+
+public class PropertyService {
+    
+    @Autowired
+    private PropertyRepository propertyRepository;
+    
+    /*
+    @Autowired
+    private PrestacionesRepository prestacionesRepository;
+    @Autowired
+    private ImageRepository imageRepository;
+    @Autowired
+    private OwnerRepository owerRepository;
+    */
+    
+    @Transactional
+    public void crearProperty(String idPrestaciones, String idImagenes,String idPropietario,String idComentario,Double precioBase,
+            String codigoPostal,String direccion,String descripcion , Double valoracion) throws MiException{
+        
+        validar(precioBase, codigoPostal, direccion, descripcion);
+        //Owner propietario =owerRepositoryfindById(idPropietario).get();
+        //Prestation prestaciones=prestacionesRepositoryfindById(idPrestaciones).get();
+        
+        Property propiedad=new Property();
+        propiedad.setPrecioBase(precioBase);
+        propiedad.setCodigoPostal(codigoPostal);
+        propiedad.setDescripcion(descripcion);
+        propiedad.setDireccion(direccion);
+        propiedad.setAlta(Boolean.TRUE);
+        //propiedad.setPrestaciones(prestaciones);
+        //propiedad.setPropietario(propietario);
+        //me falta cargar el roll city;
+        //prototype
+        
+    }
+         
+    
+    public void validar (Double precioBase,
+            String codigoPostal,String direccion,String descripcion ) throws MiException{
+       
+        
+        if (precioBase==null) {
+            throw new MiException("Por favor indicar el precio base por noche!Se podra modificar mas adelante !");
+        }
+        if (codigoPostal==null) {
+            throw new MiException("Tiene que indicar el Codigo Postal de la propiedad , por favor.");
+        }
+        if (direccion==null) {
+            throw new MiException("Tiene que indicar la direccion exacta de la propiedad , por favor.");
+        }
+        if ( descripcion==null) {
+            throw new MiException("Por favor debe insertar un comentario, ayudara mucho al cliente a tomar una decision.");
+        }
+       
+    }
+    
+   
+}
