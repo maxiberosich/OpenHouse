b64e3ba8542f7a77edf68f622c4522b5f40b1d79
“Jhoel <pkap1995@gmail.com>
2023-10-27 18:58:21 -0300

    Owner : propietario entidad y servicio completos.

--- src/main/java/openHouse/demo/entities/Owner.java
+++ src/main/java/openHouse/demo/entities/Owner.java
@@ -1,12 +1,26 @@
 package openHouse.demo.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import java.util.List;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
+@Data
 
-public class Owner {
+public class Owner extends User{
 
     @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String Id;
+    
+    private String cbu;
+    
+    @ManyToOne
+    private List<Property> propiedades;
+    
 }
