883449a5b665a8c1612d3e6bd2c957436b518ea5
Fernán Pietrantuono <fernanpietrantuono@gmail.com>
2023-10-31 18:31:11 -0300

    Agrego las Queries necesarias en las clases Repositorios

--- src/main/java/openHouse/demo/repositories/PropertyRepository.java
+++ src/main/java/openHouse/demo/repositories/PropertyRepository.java
@@ -2,6 +2,7 @@ package openHouse.demo.repositories;
 
 import java.util.List;
 import openHouse.demo.entities.Property;
+import openHouse.demo.enums.PropType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,8 +13,14 @@ public interface PropertyRepository extends JpaRepository<Property, String> {
 
     @Query("SELECT p FROM Property p WHERE p.codigoPostal = :codigoPostal")
     public List<Property> buscarPorCodigoPostal(@Param("codigoPostal") String codigoPostal);
-    
+
     @Query("SELECT p FROM Property p WHERE p.ciudad LIKE '%:ciudad%'")
     public List<Property> buscarPorCiudad(@Param("ciudad") String ciudad);
-    
+
+    @Query("SELECT p FROM Property p WHERE p.tipo = :tipo")
+    public List<Property> buscarPorTipo(@Param("tipo") PropType tipo);
+
+//    @Query("SELECT p FROM Property p WHERE p.alta = :alta")
+//    public List<Property> buscarPorDisponibilidad(@Param("alta") Boolean alta);
+
 }
