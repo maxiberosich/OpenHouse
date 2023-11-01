61efc3cb3d4f0740284482b1bd5787c785694247
Yazmin <118831153+YazminFerreyra23@users.noreply.github.com>
2023-11-01 01:01:36 -0300

    NO TOQUEN MI FRONT-VERIFIQUEN CUANDO MODIFICAN EL STYLE
    NO TOQUEN MI FRONT-STYLE

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,9 +1,14 @@
 const $btnSignIn = document.querySelector('.sign-in-btn'),
         $btnSignUp = document.querySelector('.sign-up-btn'),
         $signUp = document.querySelector('.sign-up'),
-        $signIn = document.querySelector('.sign-in');
+        $signIn = document.querySelector('.sign-in')
+        ;
 
 document.addEventListener('click', e => {
     if (e.target === $btnSignIn || e.target === $btnSignUp) {
         $signIn.classList.toggle('active');
+        $signUp.classList.toggle('active');
+
+    }
+
 });
