9af891f18df2e3ae60971975530515f778b10c48
Nico Colman <nico.colman02@gmail.com>
2023-10-31 23:43:43 -0300

    Agregar vistas de propiedades

---
+++ src/main/resources/static/Propiedad.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+
