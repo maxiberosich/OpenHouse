66cb294ea2483c51a16bcf9db9735bce64054fd6
“Laura <laurazeier@gmail.com>
2023-11-03 10:22:51 -0300

    oo

--- src/main/java/openHouse/demo/services/ImageService.java
+++ src/main/java/openHouse/demo/services/ImageService.java
@@ -1,6 +1,6 @@
 package openHouse.demo.services;
 
-/*import java.io.IOException;
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 import openHouse.demo.entities.Image;
@@ -63,4 +63,3 @@ public class ImageService {
     }
     
 }
-*/
