9ed059abd92432012950ffc64c55319ad5fe1109
Matias Giache <matiasgiache08@gmail.com>
2023-11-03 20:11:28 -0300

    Creacion de Reserva con Cliente y propiedad funcional, cambios con spring security para el inicio

--- src/main/java/openHouse/demo/controllers/ReservationController.java
+++ src/main/java/openHouse/demo/controllers/ReservationController.java
@@ -2,10 +2,13 @@ package openHouse.demo.controllers;
 
 import jakarta.servlet.http.HttpSession;
 import java.util.Date;
+import openHouse.demo.entities.Property;
 import openHouse.demo.entities.User;
 import openHouse.demo.exceptions.MiException;
+import openHouse.demo.services.PropertyService;
 import openHouse.demo.services.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,18 +23,26 @@ public class ReservationController {
     @Autowired 
     private ReservationService reservaServicio;
     
-    
+    @Autowired
+    private PropertyService propService;
+    //
     @GetMapping("/crear/{idPropiedad}")
     public String crearReserva(ModelMap modelo, HttpSession session,@PathVariable String idPropiedad){
         User user = (User) session.getAttribute("usersession");
         modelo.put("user", user);
-        modelo.put("idPropiedad", idPropiedad);
+        //modelo.put("idPropiedad", idPropiedad);
+        
+        Property propiedad = propService.buscarPropiedad(idPropiedad);
+        modelo.addAttribute("propiedad", propiedad);
+        
         return "reservar_propiedad.html";
     }
     
     @PostMapping("/crearReserva/{idPropiedad}/{idCliente}")
-    public String creacionReserva(ModelMap modelo,@RequestParam Date fechaInicio,@RequestParam Date fechaFin,
-            @PathVariable String idCliente, @RequestParam Integer cantPersonas,@PathVariable String idPropiedad){
+    public String creacionReserva(ModelMap modelo,@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd")
+            Date fechaInicio,@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd")
+            Date fechaFin,@PathVariable String idCliente, @RequestParam Integer cantPersonas,
+            @PathVariable String idPropiedad){
         
         try {
             reservaServicio.crearReservacion(fechaInicio, fechaFin, idCliente, cantPersonas, idPropiedad);
@@ -47,4 +58,5 @@ public class ReservationController {
             return "reservar_propiedad.html";
         }
     }
+    
 }
