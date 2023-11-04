2f1197fc6d9e5a423460ace435e6bf66b193bb49
Yazmin <118831153+YazminFerreyra23@users.noreply.github.com>
2023-11-03 23:30:28 -0300

    login-registrar

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -6,7 +6,6 @@ const $btnSignIn = document.querySelector('.sign-in-btn'),
 
 document.addEventListener('click', e => {
     if (e.target === $btnSignIn || e.target === $btnSignUp) {
-
         $signIn.classList.toggle('active');
         $signUp.classList.toggle('active');
 
