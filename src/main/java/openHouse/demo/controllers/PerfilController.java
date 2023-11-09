6f56e83389d87b3b1f4699cf63d34386254e16b7
“Jhoel <pkap1995@gmail.com>
2023-11-09 18:26:46 -0300

    toque mas que un cura

--- src/main/java/openHouse/demo/controllers/PerfilController.java
+++ src/main/java/openHouse/demo/controllers/PerfilController.java
@@ -2,6 +2,7 @@ package openHouse.demo.controllers;
 
 import openHouse.demo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,21 +15,9 @@ public class PerfilController {
     private UserService userService;
 
     @PreAuthorize("hasAnyRole('ROLE_USER','ROLE_ADMIN')")
-    @GetMapping("/perfil/{id}")
-    public String perfil(ModelMap modelo, HttpSession session) {
-        User user = (User) session.getAttribute("usersession");
-        if (user != null) {
-            modelo.put("user", user);
-            return "perfil.html";
-        } else {
-            return "inicio.html";
-        }
-    }
-
-        
+    @GetMapping("/perfil")
+    public String perfil() {
         return "perfil.html";
     }
-    
-    
-    
+
 }
